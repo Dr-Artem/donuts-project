b2ae96a0a2c90d17ea596bb444067af54a497e45
Dr-Artem <77886211+Dr-Artem@users.noreply.github.com>
2022-11-20 22:30:54 +0000

    Deploying to gh-pages from @ Dr-Artem/donuts-project@dfcd62224883703b76445cd5f75240453a68e8d7 🚀

---
+++ index.cefdc6d2.js
@@ -0,0 +1,2 @@
+var mybutton=document.getElementById("toTop");function scrollFunction(){document.body.scrollTop>300||document.documentElement.scrollTop>300?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceMappingURL=index.cefdc6d2.js.map

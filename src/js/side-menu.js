bd4998272e384411285461bfc26a0f4796122938
Dr-Artem <artic12101997dr@gmail.com>
2023-09-21 19:41:34 +0300

    Fixed bugs which had been there from the start

--- src/js/side-menu.js
+++ src/js/side-menu.js
@@ -1,19 +1,25 @@
 (() => {
-	const refs = {
-		openMenuBtn: document.querySelector("[data-menu-open]"),
-		closeMenuBtn: document.querySelector("[data-menu-close]"),
-		menu: document.querySelector("[data-menu]"),
-		cover: document.querySelector("[data-cover]"),
+    const refs = {
+        openMenuBtn: document.querySelector('[data-menu-open]'),
+        closeMenuBtn: document.querySelector('[data-menu-close]'),
+        menu: document.querySelector('[data-menu]'),
+        cover: document.querySelector('[data-cover]'),
+        links: document.querySelectorAll('.navigation__link'),
     };
 
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    if (window.innerWidth < 1280) {
+        refs.links.forEach(link => {
+            link.addEventListener('click', toggleMenu);
+        });
+    }
 
-	function toggleMenu() {
-		refs.openMenuBtn.classList.toggle("open");
-		refs.closeMenuBtn.classList.toggle("close");
-		refs.cover.classList.toggle("cover");
-		refs.menu.classList.toggle("is-hide");
-		document.querySelector('body').classList.toggle('is-hidden');
+    function toggleMenu() {
+        refs.openMenuBtn.classList.toggle('open');
+        refs.closeMenuBtn.classList.toggle('close');
+        refs.cover.classList.toggle('cover');
+        refs.menu.classList.toggle('is-hide');
+        document.querySelector('body').classList.toggle('is-hidden');
     }
 })();
